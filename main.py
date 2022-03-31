be5904b8333a073f9c604749eb2b040616626ca1
sharik-game <vova2008razor@yandex.ru>
2022-03-31 14:26:11 +0300

    3

---
+++ main.py
@@ -0,0 +1,10 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class SubsequenceApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, World", halign="center")
+
+
+SubsequenceApp().run()
