e3f86e6a846d21984c9897f87458ecd4a13bfc88
sharik-game <vova2008razor@yandex.ru>
2022-04-07 20:23:50 +0300

    New modification 2

--- main.py
+++ main.py
@@ -1,10 +1,72 @@
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatIconButton
+from kivymd.uix.button import MDIconButton
+# импортирование необходимых модулей
 
 
-class SubsequenceApp(MDApp):
+
+
+
+class Guess_subsequence(MDApp):
+    """ Главный класс"""
+
+
+
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        """Создание виджетов (кнопок).
+           Главная функция.
+        """
+        screen = MDScreen()
 
 
-SubsequenceApp().run()
+
+
+        def settings():
+            """ Эта функция вызывается при нажатии на кнопку настройки.
+                В ней будут такие настройки как: звук, магазин,о разработчике, язык(English, Russian).
+            """
+            screen.clear_widgets()
+            print(7)
+        settings_call = lambda sc: settings() # это лямбда функция передаёт функцию с настройками в переменную нажатия кнопки
+        # self.theme_cls.material_style = "M3"
+        screen.add_widget(
+            MDRectangleFlatIconButton(
+                # easy level
+                text="easy",
+                icon="language-python",
+                line_color=(0, 0, 0, 0),
+                pos_hint={"center_x": .5, "center_y": .6},
+            )
+        )
+
+
+        screen.add_widget(
+            MDRectangleFlatIconButton(
+                # normal level
+                text="normal",
+                icon="language-python",
+                line_color=(0, 0, 0, 0),
+                pos_hint={"center_x": .5, "center_y": .5},
+            )
+        )
+        screen.add_widget(
+            MDRectangleFlatIconButton(
+                # hard level
+                text="hard",
+                icon="language-python",
+                line_color=(0, 0, 0, 0),
+                pos_hint={"center_x": .5, "center_y": .4},
+            )
+        )
+        screen.add_widget(
+            MDIconButton(
+                # This is settings
+                icon="language-python",
+                pos_hint={"center_x": .9, "center_y": .9},
+                on_press=settings_call
+            )
+        )
+
+        return screen
+Guess_subsequence().run() # вызов главного класса
